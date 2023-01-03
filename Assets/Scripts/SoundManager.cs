f7ade11e94759bb97294842431bdc48ff5ed4e4e
Michał <michalprotas00@gmail.com>
2023-01-03 23:36:56 +0100

    Sound manager
    Basic aspects of sound manager such as:
    - playing music
    - playing sound
    Partly done options panel in main menu

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    [SerializeField]
+    private AudioSource MusicSource, EffectSource;
+
+    [SerializeField]
+    private AudioClip[] MusicClips, EffectClips;
+
+    private void Awake()
+    {
+        // making sure that SoundManager is singleton
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        if(SceneManager.GetActiveScene().name == "Menu")
+        {
+            PlayMusic(MusicClips[0]);
+        }
+
+        SceneManager.sceneLoaded += ChangeMusic;
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        EffectSource.PlayOneShot(clip);
+    }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        MusicSource.clip = clip;
+        MusicSource.Play();
+    }
+
+    public void ChangeMusic(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("Scene changed");
+        if(scene.buildIndex > 0)
+        {
+            PlayMusic(MusicClips[1]);
+        }
+    }
+}
